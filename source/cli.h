d700309363f965c1171ebeb29fe74a0d6319c281
cepthomas <cepthomas@gmail.com>
2024-02-03 12:12:02 -0600

    Relocate general cli function to cbot.

--- source/cli.h
+++ source/cli.h
@@ -6,41 +6,66 @@
 
 //---------------- Public API ----------------------//
 
+//---------------- Types ---------------------------//
+
+/// Flavor of connection.
+typedef enum { IF_NONE, IF_STDIO, IF_SOCKET, IF_SERIAL } iftype_t;
+
 /// Convenience container for processed args.
-#define MAX_CLI_ARGS 10
-#define MAX_CLI_ARG_LEN  32 // incl terminator
+#define MAX_CLI_ARGS    10
+#define MAX_CLI_ARG_LEN 32 // incl terminator
 typedef struct
 {
-    // how many arg_values. 0 indicates error string in arg_values[0].
+    /// How many arg_values. 0 indicates error string in arg_values[0].
     int arg_count;
+    /// The actual values as strings.
     char arg_values[MAX_CLI_ARGS][MAX_CLI_ARG_LEN];
 } cli_args_t;
 
+/// Cli command descriptor.
+typedef struct cli_command_desc
+{
+    /// If you like to type.
+    const char* long_name;
+    /// If you don't.
+    const char* short_name;
+    /// Optional single char for immediate execution (no return). Can be prefixed with ^(ctrl) or ~(alt).
+    const char* immediate_key;
+    /// Free text for description.
+    const char* info;
+    /// Free text for description.
+    const char* args;
+} cli_command_desc_t;
+
+// Cli command handler.
+typedef int (* cli_command_handler_t)(const cli_command_desc_t* pcmd, cli_args_t* args);
+
+/// Bind a cli command to its handler.
+typedef struct cli_command
+{
+    const cli_command_handler_t handler;
+    const cli_command_desc_t desc;
+} cli_command_t;
+
+
+//---------------- Functions -----------------------//
 
 /// Open a cli using stdio.
 /// @return status.
-int cli_OpenStdio(void);
-
-/// Open a cli using socket.
-/// @param host
-/// @param port
-/// @return status.
-int cli_OpenSocket(const char* host, int port);
-
-/// Open a cli using serial port.
-/// @param port
-/// @param baudrate
-/// @return status.
-int cli_OpenSerial(int port, int baudrate);
+int cli_Open(iftype_t iftype, cli_command_t _commands[]);
 
 /// Clean up component resources.
 /// @return status.
 int cli_Destroy(void);
 
-/// Read a line from a cli. This does not block. Buffers chars until EOL.
-/// @param args User supplied collection.
-/// @return status | ENODATA.
-int cli_ReadLine(cli_args_t* args);
+/// Cycle the periodic cli processing.
+/// @return status.
+int cli_Pump(void);
+
+// /// Read a line from a cli. This does not block. Buffers chars until EOL.
+// /// @param args User supplied collection.
+// /// @return status | ENODATA.
+// int cli_ReadLine(cli_args_t* args);
 
 /// Write a line to a cli.
 /// @param buff Line to send to user.
--- source/private/cli.c
+++ source/private/cli.c
@@ -9,19 +9,15 @@
 #include "cli.h"
 
 
-// TODO1 handle immediate key e.g. space bar to start/stop, maybe ctrl-space?
-// [history](C:\Dev\AL\harvester\xib-firmware\vendor\appliedlogix\cli-library\cli\cli_interpreter.c)
+// TODO1 [history](C:\Dev\AL\harvester\xib-firmware\vendor\appliedlogix\cli-library\cli\cli_interpreter.c)
 
-// TODO3 support nx, probably ncurses.
-// GetAsyncKeyState
+// FUTURE support nx, maybe ncurses.
 
 
 //---------------- Private ------------------------------//
 
 /// Max raw line.
 #define CLI_BUFF_LEN 128
-
-typedef enum { IF_NONE, IF_STDIO, IF_SOCKET, IF_SERIAL } ifType_t;
 
 /// CLI buffer to collect input chars.
 static char _cli_buff[MAX_CLI_ARGS * MAX_CLI_ARG_LEN];
@@ -33,68 +29,57 @@ static int _buff_index = -1;
 static char* _prompt = "";
 
 /// The flavor.
-static ifType_t _iftype = IF_NONE;
+static iftype_t _iftype = IF_NONE;
+
+/// The command list.
+static cli_command_t* _commands;
+
+/// Local function.
+static int _ReadLine(cli_args_t* args);
+
+/// Local function.
+static int _Usage(void);
 
 
 //---------------- API Implementation -----------------//
 
 
 //--------------------------------------------------------//
-int cli_OpenStdio(void)
+int cli_Open(iftype_t iftype, cli_command_t commands[])
 {
     int stat = CBOT_ERR_NO_ERR;
-    _buff_index = -1;
-    _iftype = IF_STDIO;
-    _prompt = "$";
 
+    _iftype = iftype;
+    _commands = commands;
+    _buff_index = -1;
     memset(_cli_buff, 0, CLI_BUFF_LEN);
 
-    // Prompt.
-    cli_WriteLine("");
-    return stat;
-}
-
-
-//--------------------------------------------------------//
-int cli_OpenSocket(const char* host, int port) //FUTURE
-{
-    int stat = CBOT_ERR_NO_ERR;
-    _buff_index = -1;
-    _iftype = IF_SOCKET;
-    _prompt = "";
-
-    memset(_cli_buff, 0, CLI_BUFF_LEN);
-
-    // Prompt.
-    cli_WriteLine("");
-
-    // // Connect.
-    // struct hostent* h = gethostbyname(host);
-    // if(!h) { return 1; }
-    // char* ip = inet_ntoa(*(struct in_addr*)h->h_addr);
-    // _fd = socket(AF_INET, SOCK_STREAM, 0);
-    // if (_fd < 0) { return 2; }
-    // struct sockaddr_in saddr;
-    // memset(&saddr, 0, sizeof(saddr));
-    // saddr.sin_family = AF_INET;
-    // saddr.sin_port = htons(port);
-    // inet_aton(ip, &saddr.sin_addr);
-    // stat = connect(fd, (struct sockaddr*)&saddr, sizeof(saddr));
-    // if (stat < 0) { return 3; }
-
-    return stat;
-}
-
-
-//--------------------------------------------------------//
-int cli_OpenSerial(int port, int baudrate) // FUTURE
-{
-    int stat = CBOT_ERR_NO_ERR;
-    _buff_index = -1;
-    _iftype = IF_SERIAL;
-    _prompt = "$";
-
-    memset(_cli_buff, 0, CLI_BUFF_LEN);
+    switch(_iftype)
+    {
+        case IF_STDIO:
+            _prompt = "$";
+            break;
+        case IF_SOCKET:
+            // // Connect.
+            // struct hostent* h = gethostbyname(host);
+            // if(!h) { return 1; }
+            // char* ip = inet_ntoa(*(struct in_addr*)h->h_addr);
+            // _fd = socket(AF_INET, SOCK_STREAM, 0);
+            // if (_fd < 0) { return 2; }
+            // struct sockaddr_in saddr;
+            // memset(&saddr, 0, sizeof(saddr));
+            // saddr.sin_family = AF_INET;
+            // saddr.sin_port = htons(port);
+            // inet_aton(ip, &saddr.sin_addr);
+            // stat = connect(fd, (struct sockaddr*)&saddr, sizeof(saddr));
+            // if (stat < 0) { return 3; }
+            break;
+        case IF_SERIAL:
+            _prompt = "$";
+            break;
+        default:
+            break;
+    }
 
     // Prompt.
     cli_WriteLine("");
@@ -126,13 +111,129 @@ int cli_Destroy(void)
 
 
 //--------------------------------------------------------//
-int cli_ReadLine(cli_args_t* args)
+int cli_Pump(void)
 {
+    int stat = CBOT_ERR_NO_ERR;
+    int cbot_stat = CBOT_ERR_NO_ERR;
+    cli_args_t cli_args;
+
+    cbot_stat = _ReadLine(&cli_args);
+    // _EvalStatus(cbot_stat, "Failed to _ReadLine");
+
+    switch (cbot_stat)
+    {
+    case CBOT_ERR_NO_ERR: // something to do
+        bool valid = false;
+
+        // Process the command and its options.
+        if (cli_args.arg_count > 0)
+        {
+            // Find and execute the command.
+            const cli_command_t* pcmd = _commands;
+            while (pcmd->handler != NULL)
+            {
+                if (strcmp(pcmd->desc.short_name, cli_args.arg_values[0]) || strcmp(pcmd->desc.long_name, cli_args.arg_values[0]))
+                {
+                    valid = true;
+                    // Lock access to lua context.
+                    //ENTER_CRITICAL_SECTION;
+                    // Execute the command. They handle any errors internally.
+                    stat = (*pcmd->handler)(&(pcmd->desc), &cli_args);
+                    // _EvalStatus(stat, "handler failed: %s", pcmd->desc.long_name);
+                    //EXIT_CRITICAL_SECTION;
+                    break;
+                }
+            }
+
+            if (!valid)
+            {
+                cli_WriteLine("invalid command");
+            }
+        }
+        break;
+
+    case CBOT_ERR_NO_DATA: // nothing to do
+        break;
+
+    default: // error
+        cbot_stat = cli_WriteLine(cli_args.arg_values[0]);
+        // _EvalStatus(cbot_stat, "cli_WriteLine() error");
+        break;
+    }
+
+    return stat;
+}
+
+
+//--------------------------------------------------------//
+int cli_WriteLine(const char* format, ...)
+{
+    int stat = CBOT_ERR_NO_ERR;
+
+    static char buff[CLI_BUFF_LEN];
+
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buff, CLI_BUFF_LEN-1, format, args);
+    va_end(args);
+
+    switch(_iftype)
+    {
+        case IF_STDIO:
+            printf("%s\r\n>", buff);
+            break;
+        case IF_SOCKET:
+            break;
+        case IF_SERIAL:
+            stat = 1;
+            break;
+        default:
+            stat = 1;
+            break;
+    }
+
+    return stat;    
+}
+
+
+//--------------------------------------------------------//
+int cli_WriteChar(char c)
+{
+    int stat = CBOT_ERR_NO_ERR;
+
+    switch(_iftype)
+    {
+        case IF_STDIO:
+            putchar(c);
+            break;
+        case IF_SOCKET:
+            // fputc();
+            break;
+        case IF_SERIAL:
+            stat = 1;
+            break;
+        default:
+            stat = 1;
+            break;
+    }
+    
+    return stat;    
+}
+
+
+//--------------------------------------------------------//
+int _ReadLine(cli_args_t* args)
+{
+    // TODO1 handle immediate key e.g. space bar to start/stop, maybe ctrl-space? 
+    // TODO1 { _Usage, { "help",       "?",   "",   "explain it all", "" } },
+
     bool line_done = false;
     char* serr = NULL;
 
     // Process each new char.
     char c = -1;
+    bool ctrl = false;
+    bool alt = false;
     bool chars_done = false;
 
     while (!chars_done)
@@ -141,7 +242,15 @@ int cli_ReadLine(cli_args_t* args)
         {
             case IF_STDIO:
                 {
-                    c = _kbhit() ? (char)_getch() : -1;
+                    if (_kbhit())
+                    {
+                        c = (char)_getch();
+                        //TODO1 ctrl/alt? GetAsyncKeyState()
+                    }
+                    else
+                    {
+                        c = -1;
+                    }
                 }
                 break;
             case IF_SOCKET:
@@ -161,6 +270,7 @@ int cli_ReadLine(cli_args_t* args)
                 break;
         }
 
+        // What happened?
         switch(c)
         {
             case -1:
@@ -234,56 +344,29 @@ int cli_ReadLine(cli_args_t* args)
 
 
 //--------------------------------------------------------//
-int cli_WriteLine(const char* format, ...)
+int _Usage(void)
 {
     int stat = CBOT_ERR_NO_ERR;
 
-    static char buff[CLI_BUFF_LEN];
-
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buff, CLI_BUFF_LEN-1, format, args);
-    va_end(args);
-
-    switch(_iftype)
+    const cli_command_t* cmditer = _commands;
+    while (_commands->handler != NULL_PTR)
     {
-        case IF_STDIO:
-            printf("%s\r\n>", buff);
-            break;
-        case IF_SOCKET:
-            break;
-        case IF_SERIAL:
-            stat = 1;
-            break;
-        default:
-            stat = 1;
-            break;
+        const cli_command_desc_t* pdesc = &(cmditer->desc);
+        cli_WriteLine("%s|%s: %s", pdesc->long_name, pdesc->short_name, pdesc->info);
+        if (strlen(pdesc->args) > 0)
+        {
+            // Maybe multiline args. Make writable copy and tokenize it.
+            char cp[128];
+            strncpy(cp, pdesc->args, sizeof(cp));
+            char* tok = strtok(cp, "$");
+            while (tok != NULL)
+            {
+                cli_WriteLine("    %s", tok);
+                tok = strtok(NULL, "$");
+            }
+        }
+        cmditer++;
     }
 
-    return stat;    
-}
-
-
-//--------------------------------------------------------//
-int cli_WriteChar(char c)
-{
-    int stat = CBOT_ERR_NO_ERR;
-
-    switch(_iftype)
-    {
-        case IF_STDIO:
-            putchar(c);
-            break;
-        case IF_SOCKET:
-            // fputc();
-            break;
-        case IF_SERIAL:
-            stat = 1;
-            break;
-        default:
-            stat = 1;
-            break;
-    }
-    
-    return stat;    
+    return stat;
 }
